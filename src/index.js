e5187816679ae1ff3079cccf664bbda8201bb8ec
NikitaVirachev <guardsolder@gmail.com>
2019-10-28 16:05:54 +0300

    add calendar

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+/*function App() {
+    return (
+    <div>cgbhjk
+    <Welcome name="Sara" />
+    <Welcome name="Cahal" />
+    <Welcome name="Edite" />
+    </div>
+    );
+}*/
+    
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,28 @@ module.exports = {
     hot: true,
     historyApiFallback: true
   },*/
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+    ]
+  },
   plugins: [
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    }),
     new CleanWebpackPlugin()
   ]
 };
