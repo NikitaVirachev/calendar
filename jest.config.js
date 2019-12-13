a20978375e8970b93a82f53e3fa3eee38d5b845b
NikitaVirachev <guardsolder@gmail.com>
2019-12-13 10:39:08 +0300

    installation jest and enzyme

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    verbose: true,
+    setupFiles: ['./jest.setup.js'],
+    setupFilesAfterEnv: ['./jest.setup-after-env.js'],
+    transform: {
+      '^.+\\.js$': 'babel-jest'
+    },
+    moduleNameMapper: {
+      'src/(.*)': '<rootDir>/src/$1',
+      '\\.css$': '<rootDir>/__mocks__/styleMock.js'
+    }
+  };
